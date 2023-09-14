750c3e034629e1e9ce5bfbf55ce67ee3014b9183
SURESH <SURESH@LAPPY>
2023-09-15 00:16:45 +0530

    Day1 programs

---
+++ Day1/src/com/svit/day1/IdentifersDemo.java
@@ -0,0 +1,15 @@
+package com.svit.day1;
+
+public class IdentifersDemo {
+	public static void main(String[] args) {
+		
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Rashmika"; 
+		System.out.println("value is : "+ studentName);
+		
+	
+	}
+
+}
