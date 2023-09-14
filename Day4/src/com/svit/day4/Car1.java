fb29816577c425e19e15c7cf7fe956bf689aa80f
SURESH <SURESH@LAPPY>
2023-09-15 00:36:51 +0530

    Day4 programs

---
+++ Day4/src/com/svit/day4/Car1.java
@@ -0,0 +1,8 @@
+package com.svit.day4;
+
+public class Car1 {
+	private String doors;
+	private String engine;
+	private String drivers;
+	public int speed ;
+}
---
+++ Day4/src/com/svit/day4/Main3getterssettersAddfunctionality.java
@@ -0,0 +1,12 @@
+package com.svit.day4;
+
+public class Main3getterssettersAddfunctionality {
+	public static void main(String[] args) {
+		Car3getterssetterAddfunctinality car = new Car3getterssetterAddfunctinality ();
+		car.setSpeed (10);
+		car.setDoors ("closed");
+		car.setEngine ("on");
+		car.setDrivers ("seated");		
+		System.out.println (car.run ());
+	}
+}
